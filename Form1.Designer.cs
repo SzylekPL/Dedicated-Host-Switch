1c73922007ca1d960e61047da4de2a1ee29e1de7
SzylekPL <77230982+SzylekPL@users.noreply.github.com>
2023-09-26 19:23:16 +0200

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Dedicated_Host_Switch
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnTrue = new Button();
+            btnFalse = new Button();
+            btnToggle = new Button();
+            btnChangeFile = new Button();
+            SuspendLayout();
+            // 
+            // btnTrue
+            // 
+            btnTrue.Location = new Point(12, 13);
+            btnTrue.Name = "btnTrue";
+            btnTrue.Size = new Size(111, 27);
+            btnTrue.TabIndex = 0;
+            btnTrue.Text = "Set to True";
+            btnTrue.UseVisualStyleBackColor = true;
+            btnTrue.Click += btnTrue_Click;
+            // 
+            // btnFalse
+            // 
+            btnFalse.Location = new Point(129, 12);
+            btnFalse.Name = "btnFalse";
+            btnFalse.Size = new Size(111, 27);
+            btnFalse.TabIndex = 1;
+            btnFalse.Text = "Set to False";
+            btnFalse.UseVisualStyleBackColor = true;
+            btnFalse.Click += btnFalse_Click;
+            // 
+            // btnToggle
+            // 
+            btnToggle.Location = new Point(246, 13);
+            btnToggle.Name = "btnToggle";
+            btnToggle.Size = new Size(111, 27);
+            btnToggle.TabIndex = 2;
+            btnToggle.Text = "Toggle State";
+            btnToggle.UseVisualStyleBackColor = true;
+            btnToggle.Click += btnToggle_Click;
+            // 
+            // btnChangeFile
+            // 
+            btnChangeFile.Location = new Point(12, 59);
+            btnChangeFile.Name = "btnChangeFile";
+            btnChangeFile.Size = new Size(345, 36);
+            btnChangeFile.TabIndex = 3;
+            btnChangeFile.Text = "Change File";
+            btnChangeFile.UseVisualStyleBackColor = true;
+            btnChangeFile.Click += btnChangeFile_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(376, 106);
+            Controls.Add(btnChangeFile);
+            Controls.Add(btnToggle);
+            Controls.Add(btnFalse);
+            Controls.Add(btnTrue);
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Dedicated Host Switch";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button btnTrue;
+        private Button btnFalse;
+        private Button btnToggle;
+        private Button btnChangeFile;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+namespace Dedicated_Host_Switch
+{
+	internal static class Program
+	{
+		
+		[STAThread]
+		static void Main()
+		{
+			ApplicationConfiguration.Initialize();
+			Application.Run(new Form1());
+
+		}
+	}
+}
