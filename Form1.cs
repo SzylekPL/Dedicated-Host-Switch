1d7b85cbdd8bd7451f64c6e8f906ef2bf1807394
SzylekPL <77230982+SzylekPL@users.noreply.github.com>
2023-12-07 17:55:14 +0100

    Rebuilt the entire project, similar to the console version of the app.

--- Form1.cs
+++ Form1.cs
@@ -1,142 +1,49 @@
 namespace Dedicated_Host_Switch
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-        public static class SaveFile
-        {
-            public static readonly string[] Line = { "\tDedicatedHost = false,", "\tDedicatedHost = true,", "Dedicated host has been set to \"false\".", "Dedicated host has been set to \"true\"." };
-            private static readonly FileInfo SaveFileInfo = new(Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "save.csv"));
-            private static string? LuaFilePath;
-            public static string[]? LuaContent;
-            public static void Initialise()
-            {
-                if (!SaveFileInfo.Exists)
-                    SaveFileInfo.Create();
-                if (SaveFileInfo.Length == 0)
-                    LuaFileSelection();
-                LuaFilePath = File.ReadAllText(SaveFileInfo.FullName);
-                LuaContent = File.ReadAllLines(LuaFilePath);
-            }
-            public static void LuaSave() => File.WriteAllLines(LuaFilePath, LuaContent);
-            public static void LuaFileSelection()
-            {
-                OpenFileDialog FileSelection = new OpenFileDialog();
-                FileSelection.Title = "File Selection";
-                FileSelection.Filter = "Lua Source File (*.lua)|*.lua";
-                if (FileSelection.ShowDialog() == DialogResult.OK)
-                    LuaFilePath = FileSelection.FileName;
-                File.WriteAllText(SaveFileInfo.FullName, LuaFilePath);
-            }
-        }
-        private void Form1_Load(object sender, EventArgs e) => SaveFile.Initialise();
-        private void btnTrue_Click(object sender, EventArgs e)
-        {
-            SaveFile.LuaContent[11] = SaveFile.Line[1];
-            SaveFile.LuaSave();
-        }
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
 
-        private void btnFalse_Click(object sender, EventArgs e)
-        {
-            SaveFile.LuaContent[11] = SaveFile.Line[0];
-            SaveFile.LuaSave();
-        }
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			if (SaveFile.Empty)
+			{
+				while (LuaFileOpenDialog.ShowDialog() != DialogResult.OK) ;
+				SaveFile.Content = LuaFileOpenDialog.FileName;
+				LuaFile.Initialize(LuaFileOpenDialog.FileName);
+				return;
+			}
+			LuaFile.Initialize();
+		}
+		private void TrueButtonHandler(object sender, EventArgs e)
+		{
+			LuaFile.DedicatedHost = true;
+			LuaFile.Save();
+		}
 
-        private void btnToggle_Click(object sender, EventArgs e)
-        {
-            if (SaveFile.LuaContent[11] == SaveFile.Line[0])
-                SaveFile.LuaContent[11] = SaveFile.Line[1];
-            else
-                SaveFile.LuaContent[11] = SaveFile.Line[0];
-        }
+		private void FalseButtonHandler(object sender, EventArgs e)
+		{
+			LuaFile.DedicatedHost = false;
+			LuaFile.Save();
+		}
 
-        private void btnChangeFile_Click(object sender, EventArgs e) => SaveFile.LuaFileSelection();
+		private void ToggleButtonHandler(object sender, EventArgs e)
+		{
+			LuaFile.DedicatedHost = !LuaFile.DedicatedHost;
+			LuaFile.Save();
+		}
 
-    }
+		private void ChangeFileButtonHandler(object sender, EventArgs e)
+		{
+			if (LuaFileOpenDialog.ShowDialog() == DialogResult.OK)
+			{
+				SaveFile.Content = LuaFileOpenDialog.FileName;
+				LuaFile.Initialize(LuaFileOpenDialog.FileName);
+			}
+		}
+
+	}
 }
-
-/*class Program
-{
-	static class SaveFile
-	{
-		private static readonly FileInfo SaveFileInfo = new(Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "save.csv"));
-		private static string? LuaFilePath;
-		public static string[]? LuaContent;
-		static SaveFile()
-		{
-			if (!SaveFileInfo.Exists)
-				SaveFileInfo.Create();
-			if (SaveFileInfo.Length == 0)
-			{
-				while (!File.Exists(LuaFilePath))
-				{
-					Console.WriteLine("Save file is empty or Lua file doesn't exist, insert Lua file path.");
-					LuaFilePath = Console.ReadLine();
-				}
-				File.WriteAllText(SaveFileInfo.FullName, LuaFilePath);
-			}
-			LuaFilePath = File.ReadAllText(SaveFileInfo.FullName);
-			LuaContent = File.ReadAllLines(LuaFilePath);		}
-		public static void LuaSave() => File.WriteAllLines(LuaFilePath, LuaContent);
-		public static bool Save(string? path)
-		{
-			if (!File.Exists(path))
-				return false;
-			else
-			{
-				LuaFilePath = path;
-				File.WriteAllText(SaveFileInfo.FullName, LuaFilePath);
-				return true;
-			}
-		}
-	}
-	static void Main()
-	{
-		string[] Line = { @"	DedicatedHost = false,", @"	DedicatedHost = true,", "Dedicated host has been set to \"false\".", "Dedicated host has been set to \"true\"." };
-		byte MainLoop = 0;
-		bool LoadedState = SaveFile.LuaContent[11] == Line[1];
-		Console.WriteLine("Dedicated Host Switch\n1.True\n2.False\n3.Toggle\n4.Change Lua file path\n5.Exit");
-		while (MainLoop < 5)
-		{
-			while (!byte.TryParse(Console.ReadLine(), out MainLoop))
-				Console.WriteLine("Incorrect value, type again:");
-
-			switch (MainLoop)
-			{
-				case 1:
-					SaveFile.LuaContent[11] = Line[1];
-					SaveFile.LuaSave();
-					Console.WriteLine(Line[3]);
-					break;
-				case 2:
-					SaveFile.LuaContent[11] = Line[0];
-					SaveFile.LuaSave();
-					Console.WriteLine(Line[2]);
-					break;
-				case 3:
-					LoadedState = !LoadedState;
-					if (LoadedState)
-					{
-						SaveFile.LuaContent[11] = Line[1];
-						Console.WriteLine(Line[3]);
-					}
-					else
-					{
-						SaveFile.LuaContent[11] = Line[0];
-						Console.WriteLine(Line[2]);
-					}
-					SaveFile.LuaSave();
-					break;
-				case 4:
-					Console.WriteLine("Insert new path:");
-					while (!SaveFile.Save(Console.ReadLine()))
-						Console.WriteLine("File with this path doesn't exist, try again:");
-					Console.WriteLine("Lua file path has been updated.");
-					break;
-			}
-		}
-	}
-}*/
---
+++ LuaFile.cs
@@ -0,0 +1,26 @@
+﻿namespace Dedicated_Host_Switch
+{
+	internal static class LuaFile
+	{
+		private static string? Path;
+		private static string[] Content;
+		internal static bool DedicatedHost
+		{
+			get => Content[11] == @"	DedicatedHost = true,";
+			set
+			{
+				if (value)
+					Content[11] = @"	DedicatedHost = true,";
+				else
+					Content[11] = @"	DedicatedHost = false,";
+			}
+		}
+		internal static void Initialize(string? arg = null)
+		{
+			arg ??= SaveFile.Content;
+			Path = arg;
+			Content = File.ReadAllLines(Path);
+		}
+		internal static void Save() => File.WriteAllLines(Path, Content);
+	}
+}
